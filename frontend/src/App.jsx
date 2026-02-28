661111937fdaa9e61227673743002de6ad6cfeda
Ayush3008Anand <ayushanand3008@gmail.com>
2026-03-01 00:43:13 +0530

    job route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Homepage from "./HomePage/Homepage"
 import Client from "./AuthSection/Client/Client"
 import Engineer from "./AuthSection/Engineer/Engineer"
+import JobPage from "./JobPage/JobPage"
 
 const App=()=>{
   return(
@@ -9,6 +10,7 @@ const App=()=>{
       <Route path="/" element={<Homepage></Homepage>}></Route>
       <Route path="/choice/client" element={<Client></Client>}></Route>
       <Route path="/choice/engineer" element={<Engineer></Engineer>}></Route>
+      <Route path="/job" element={<JobPage></JobPage>}></Route>
     </Routes>
   )
 }
